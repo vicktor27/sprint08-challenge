c853f900995e24dce15bf1da7b7943121571985d
AlejandraSichaca <aleja.sichaca@hotmail.com>
2025-10-23 20:04:52 -0500

    Array

---
+++ ale.js
@@ -0,0 +1,7 @@
+const alumnos = [
+  { nombre: "Ana", calificacion: 95 },
+  { nombre: "Luis", calificacion: 72 },
+  { nombre: "Carla", calificacion: 60 },
+  { nombre: "Miguel", calificacion: 48 },
+  { nombre: "Sofia", calificacion: 88 },
+];
