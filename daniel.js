6289f0f50b12aed411dd101de2f351659dcc2c25
daniel-fernandez25 <danielfb0554@gmail.com>
2025-10-23 19:06:02 -0600

    add array list of students

---
+++ daniel.js
@@ -0,0 +1,24 @@
+const listaAlumnos = [
+  {
+    nanme: "Ana",
+    grade: 95,
+  },
+  {
+    nanme: "Luis",
+    grade: 72,
+  },
+  {
+    nanme: "Carla",
+    grade: 60,
+  },
+  {
+    nanme: "Miguel",
+    grade: 48,
+  },
+  {
+    nanme: "Sofía",
+    grade: 88,
+  },
+];
+
+//min 70
