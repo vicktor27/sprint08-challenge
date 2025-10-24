4440699511876a64adc6271e51344004d6b96ada
Maggie Gutierrez <maggie.vibes@icloud.com>
2025-10-23 19:11:24 -0600

    modified

--- maggie.js
+++ maggie.js
@@ -1,1 +1,7 @@
-console.log("hola");
+const alumnos = [
+  { nombre: "Ana", calificacion: 95 },
+  { nombre: "Luis", calificacion: 72 },
+  { nombre: "Carla", calificacion: 60 },
+  { nombre: "Miguel", calificacion: 48 },
+  { nombre: "Sofia", calificacion: 88 },
+];
